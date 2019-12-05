e978cb578866026419b6a9c3351c63cc0d155903
ben33779610 <49440361+ben33779610@users.noreply.github.com>
2019-12-05 21:40:10 +0800

    練習

--- UnityProject/Assets/Scripts/Black.cs
+++ UnityProject/Assets/Scripts/Black.cs
@@ -4,6 +4,8 @@ using UnityEngine.UI;
 public class Black : MonoBehaviour
 {
     #region 練習區域 - 在此區域內練習
+	
+
     
     #endregion
 }
--- UnityProject/Assets/Scripts/White.cs
+++ UnityProject/Assets/Scripts/White.cs
@@ -2,8 +2,33 @@
 
 public class White : MonoBehaviour
 {
-    #region 練習區域 - 在此區域內練習
-    
+	#region 練習區域 - 在此區域內練習
+	[Header("子彈")]
+	public GameObject bullet;	
+	[Header("子彈發射位置")]
+	public Transform shootpos;
+	[Header("音效")]
+	public AudioSource aud;
+	public AudioClip audc;
+
+	/// <summary>
+	/// 射擊
+	/// </summary>
+	private void Shoot()
+	{
+		if (Input.GetKeyDown(KeyCode.Mouse0))
+		{
+			Quaternion rot = new Quaternion(0, 0, 0, 0);
+			Instantiate(bullet, shootpos.position, rot);
+		}
+	}
+
+
+
+
+
+
+
     #endregion
 
     #region KID 區域 - 不要偷看 @3@
@@ -15,11 +40,13 @@ public class White : MonoBehaviour
     private void Start()
     {
         rig = GetComponent<Rigidbody2D>();
+
     }
 
     private void FixedUpdate()
     {
         Move();
+		Shoot();
     }
 
     /// <summary>
@@ -30,4 +57,5 @@ public class White : MonoBehaviour
         rig.AddForce(transform.right * Input.GetAxisRaw("Horizontal") * speed);
     }
     #endregion
+
 }
